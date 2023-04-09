4c292b416e6bcc9ef42904e9bd2f9aaa87ec1fcd
Victor Familia <vicmfam@gmail.com>
2023-04-08 20:18:22 -0400

    completed code

--- sort.c
+++ sort.c
@@ -64,6 +64,10 @@ void merge(int arr[], int l, int m, int r)
 	int *L = (int*) malloc(n1*sizeof(int));
 	int *R = (int*) malloc(n2*sizeof(int));
 
+			{
+			extraMemoryAllocated += (sizeof(int) * n1) + (sizeof(int) * n2);
+			}
+
 	for (i = 0; i < n1; i++)
 		L[i] = arr[l + i];
 	for (j = 0; j < n2; j++)
